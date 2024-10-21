0e7754b11dd77538bd0fae01db8fe5f4facd7afe
maxim1976 <maximdongurool@gmail.com>
2024-10-21 11:48:23 +0800

    removed I

--- django_tailwind/settings.py
+++ django_tailwind/settings.py
@@ -173,7 +173,7 @@ STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-I
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'theme/static'),  # Ensure this directory also exists
 ]
